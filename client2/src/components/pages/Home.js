4563636fea96cc3046fbf53d52ae3e816311ca32
Lakhan Nad <lakhannad1999@gmail.com>
2021-06-09 01:01:35 +0530

    fix: material added

--- client2/src/components/pages/Home.js
+++ client2/src/components/pages/Home.js
@@ -1,22 +1,32 @@
-import React, { useContext, useEffect } from 'react'
-import AuthContext from '../../context/auth/authContext';
+import React, { useContext, useEffect, useState } from "react";
+import AuthContext from "../../context/auth/authContext";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+
 const Home = () => {
-
-    const authContext = useContext(AuthContext);
-    useEffect(() => {
-        authContext.loadUser();
-    }, [])
-
-    return (
-        <div className="grid-2">
-            <div>
-                Hello
-            </div>
-            <div>
-                Home Page
-            </div>
-        </div>
-    )
-}
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+    authContext.loadUser();
+  }, [authContext]);
+  const [open, setOpen] = useState(false);
+  return (
+    <React.Fragment>
+      <Box>
+        <Button onClick={() => setOpen(!open)}>Add new File</Button>
+      </Box>
+      <Dialog open={open}>
+        <DialogTitle>Adding a New File to Blockchain</DialogTitle>
+        <DialogContent>Hello</DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+};
 
 export default Home;
